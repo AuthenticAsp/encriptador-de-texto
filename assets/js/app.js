4a643e3e11b23be308ea3cf01c56025889a5282e
Alejandro <alejandropn321@gmail.com>
2024-08-22 22:02:52 -0600

    Hover en Botones y mejoras visuales

--- assets/js/app.js
+++ assets/js/app.js
@@ -23,30 +23,32 @@ function encriptarTexto() {
     leerTextoIngresado();
     if (textoIngresado === "") {
         textoEncriptado = "No se ingresó ningún texto.";
+        mostrarMensaje.innerHTML = '<div id="sinTexto" class="main__seccion-secundaria__sin-texto"><img src="assets/img/Muñeco.png" alt=""><p class="main__seccion-secundaria__texto1">Ningún mensaje fue encontrado</p><p class="main__seccion-secundaria__texto2">Ingresa el texto que desees encriptar o desencriptar.</p></div>';
     } else {
         textoEncriptado = textoIngresado.replace(/e/g, "enter")
         .replace(/i/g, "imes")
         .replace(/a/g, "ai")
         .replace(/o/g, "ober")
         .replace(/u/g, "ufat");
-    }
-    mostrarMensaje.innerHTML = `<p class="main__seccion-secundaria__texto-respuesta">${textoEncriptado}</p>
+        mostrarMensaje.innerHTML = `<p class="main__seccion-secundaria__texto-respuesta">${textoEncriptado}</p>
                                 <button onclick="copiarTexto()" class="main__seccion-secundaria__boton-copiar">Copiar</button>`;
+    }
 }
 
 function desencriptarTexto() {
     leerTextoIngresado();
     if (textoIngresado === "") {
         textoDesencriptado = "No se ingresó ningún texto.";
+        mostrarMensaje.innerHTML = '<div id="sinTexto" class="main__seccion-secundaria__sin-texto"><img src="assets/img/Muñeco.png" alt=""><p class="main__seccion-secundaria__texto1">Ningún mensaje fue encontrado</p><p class="main__seccion-secundaria__texto2">Ingresa el texto que desees encriptar o desencriptar.</p></div>';
     } else {
         textoDesencriptado = textoIngresado.replace(/enter/g, "e")
         .replace(/imes/g, "i")
         .replace(/ai/g, "a")
         .replace(/ober/g, "o")
         .replace(/ufat/g, "u");
-    }
-    mostrarMensaje.innerHTML = `<p class="main__seccion-secundaria__texto-respuesta">${textoDesencriptado}</p>
+        mostrarMensaje.innerHTML = `<p class="main__seccion-secundaria__texto-respuesta">${textoDesencriptado}</p>
                                 <button onclick="copiarTexto()" class="main__seccion-secundaria__boton-copiar">Copiar</button>`;
+    }
 }
 
 function copiarTexto() {
